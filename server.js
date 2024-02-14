5f1f6c39ebe81d61415aab77e91585a2054264a1
Elias Vinicius Raiz de Oliveira <viniciusraitz26@gmail.com>
2024-02-14 14:52:46 -0300

    Server Rodando

--- server.js
+++ server.js
@@ -8,13 +8,6 @@ const server = https.createServer({
     cert: fs.readFileSync("certificate.pem", "utf-8")
 }, app)
 
-
-const PORT_HTTPS = process.env.PORT_HTTPS || 3000
-server.listen(PORT_HTTPS, ()=>{
-    console.log(`Server Rodando em https://localhost:${PORT_HTTPS}`);
-} )
-
-
 const PORT = process.env.PORT || 3001
 app.listen(PORT, ()=>{
     console.log(`Server Rodando em http://localhost:${PORT}`);
